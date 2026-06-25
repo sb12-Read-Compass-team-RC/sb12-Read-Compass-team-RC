57830b8fde7df8fa512767d56960d32443d58962
minzy108 <minzy108@gmail.com>
2026-06-25 16:09:47 +0900

    fix: 도서 QueryDSL 구현 파일 내용 반영

--- src/main/java/com/rc/readcompass/book/repository/querydsl/BookQueryRepository.java
+++ src/main/java/com/rc/readcompass/book/repository/querydsl/BookQueryRepository.java
@@ -1,4 +1,10 @@
 package com.rc.readcompass.book.repository.querydsl;
 
-public class BookQueryRepository {
+import com.rc.readcompass.book.dto.BookDto;
+import com.rc.readcompass.book.dto.BookSearchRequest;
+import com.rc.readcompass.common.slice.SliceCursorPageResponse;
+
+public interface BookQueryRepository {
+
+  SliceCursorPageResponse<BookDto> searchCursor(BookSearchRequest request);
 }
