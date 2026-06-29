4b9f6eaf78deca313c92a764009ec81d15c18f18
Woojin-Jeong <dnwls3958@naver.com>
2026-06-29 16:04:00 +0900

    fix : unused import 제거

--- src/main/java/com/rc/readcompass/review/repository/reviewranking/ReviewRankingRepositoryCustom.java
+++ src/main/java/com/rc/readcompass/review/repository/reviewranking/ReviewRankingRepositoryCustom.java
@@ -3,10 +3,6 @@ package com.rc.readcompass.review.repository.reviewranking;
 import com.rc.readcompass.common.slice.SliceCursorPageResponse;
 import com.rc.readcompass.review.dto.PopularReviewDto;
 import com.rc.readcompass.review.dto.PopularReviewSearchRequest;
-import com.rc.readcompass.review.entity.ReviewRanking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
 
 public interface ReviewRankingRepositoryCustom {
 
