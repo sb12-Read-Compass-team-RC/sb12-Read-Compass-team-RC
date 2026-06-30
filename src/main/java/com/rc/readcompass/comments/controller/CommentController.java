7dc93297d52639fafdb10e52fed25a0949b00925
Jiye-Lee <cjstk9081@gmail.com>
2026-06-30 10:58:59 +0900

    feat: KAN-40 implement comment update controller

--- src/main/java/com/rc/readcompass/comments/controller/CommentController.java
+++ src/main/java/com/rc/readcompass/comments/controller/CommentController.java
@@ -4,6 +4,7 @@ import com.querydsl.core.types.Order;
 import com.rc.readcompass.comments.dto.CommentCreateRequest;
 import com.rc.readcompass.comments.dto.CommentDto;
 import com.rc.readcompass.comments.dto.CommentSearchRequest;
+import com.rc.readcompass.comments.dto.CommentUpdateRequest;
 import com.rc.readcompass.comments.service.CommentService;
 import com.rc.readcompass.common.slice.SliceCursorPageResponse;
 import jakarta.validation.Valid;
@@ -13,9 +14,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,6 +62,16 @@ public class CommentController {
         .limit(limit)
         .build();
     SliceCursorPageResponse<CommentDto> response = commentService.getComments(request);
+    return ResponseEntity.ok(response);
+  }
+
+  @PatchMapping("/{commentId}")
+  public ResponseEntity<CommentDto> update(
+      @PathVariable UUID commentId,
+      @RequestHeader("Deokhugam-Request-User-ID") UUID userId,
+      @Valid @RequestBody CommentUpdateRequest request
+  ){
+    CommentDto response = commentService.update(commentId, userId, request);
     return ResponseEntity.ok(response);
   }
 
